c8c9bc353b9d753da7c18ac6638e5311d7a22f49
Eric Moraes <ericmoraes1998@gmail.com>
2024-08-31 11:04:50 -0300

    Imagens no feed com preg replace

--- DankiCode/Models/HomeModel.php
+++ DankiCode/Models/HomeModel.php
@@ -7,7 +7,12 @@ class HomeModel {
         $pdo = \DankiCode\MySql::connect();
         $post = strip_tags($post);
         //TODO: substituir texto de imagem pela tag
-
+        if(preg_match('/\[imagem/',$post)){
+            $post = preg_replace('/(.*?)\[imagem=(.*?)\]/','<p>$1</p><img src=$2 />', $post);
+        }else{
+            $post = '<p>'.$post.'</p>';
+        }
+    
         //Inserindo posts na base de dados
         $postFeed = $pdo->prepare("INSERT INTO `posts` VALUES (null,?,?,?)");
         $postFeed->execute(array($_SESSION['id'],$post,date('Y-m-d H:i:s',time())));
